b1af54a207d7767e29bb9c2741af91a06722dea5
Hotte93 <74267854+Hotte93@users.noreply.github.com>
2022-12-14 15:32:53 +0100

    Resolve_Batterie_Profil_bearbeitet.py

--- Resolve_Batterie_Profil_bearbeitet.py
+++ Resolve_Batterie_Profil_bearbeitet.py
@@ -1,12 +1,11 @@
-
 from tkinter import *
 import tkinter as tk
 import time
 from pyModbusTCP.server import ModbusServer
 
 
-
-server = ModbusServer("172.16.2.190",502, no_block=True)
+counter=0
+server = ModbusServer("",502, no_block=True)
 server.start()
    
 # Create a Window
@@ -18,7 +17,6 @@ root.title("Resolve Batterie Profil")
 
 
 
-
 #Edit fields for Descrition
 #------------------------------------------------------------------------------
 IPFensterLabel=Label(root,  text="Server IP", fg="black", bg= "white")
@@ -26,6 +24,16 @@ IPFensterLabel.place(y=0, x=0, width= 100,height= 30)
     
 PortFenster =Label(root,text = "Port", fg="black", bg="white")
 PortFenster.place(y=30, x=0, width= 100, height= 30)
+
+
+IPFensterLabel=Label(root,  text="localhost", fg="black", bg= "white")
+IPFensterLabel.place(y=0, x=100, width= 200,height= 30)
+    
+PortFenster =Label(root,text = "502", fg="black", bg="white")
+PortFenster.place(y=30, x=100, width= 200, height= 30)
+
+
+
     
 SOCFenster =Label(root,text = "SOC", fg="black", bg="white")
 SOCFenster.place(y=80, x=0, width= 100, height= 30)
@@ -41,11 +49,11 @@ LadeFenster.place(y=170, x=0, width= 100, height= 30)
     
 #Edit fields for data entry
 #------------------------------------------------------------------------------
-IPEntry=Entry(root,fg="black", bg="white")
-IPEntry.place(y=0, x=100,width= 100,height= 30)
+#IPEntry=Entry(root,fg="black", bg="white")
+#IPEntry.place(y=0, x=100,width= 100,height= 30)
     
-PortEntry=Entry(root,fg="black", bg="white")
-PortEntry.place(y=30, x=100,width= 100,height= 30)
+#PortEntry=Entry(root,fg="black", bg="white")
+#PortEntry.place(y=30, x=100,width= 100,height= 30)
     
 SOCEntry=Entry(root,fg="black", bg="white")
 SOCEntry.place(y=80, x=100,width= 100,height= 30)
@@ -63,18 +71,17 @@ LadeEntry.place(y=170, x=100,width= 100,height= 30)
 #------------------------------------------------------------------------------
       
 def print_entry():
-    #server= ModbusServer("172.16.2.190",502, no_block=True)
     
     print("\n")
     print("Server is run:" + str(server.is_run)) 
     
-    print("Value before writing:" + str(server.data_bank.get_input_registers(7)))
+    print("Value before writing:" + str(server.data_bank.get_input_registers(168)))
     
-    IPFensterLabel=Label(root,  text=IPEntry.get(), fg="black", bg= "white")
-    IPFensterLabel.place(y=0, x=200, width= 100,height= 30)
+    #IPFensterLabel=Label(root,  text=IPEntry.get(), fg="black", bg= "white")
+    #IPFensterLabel.place(y=0, x=200, width= 100,height= 30)
         
-    PortFenster =Label(root,text = PortEntry.get(), fg="black", bg="white")
-    PortFenster.place(y=30, x=200, width= 100, height= 30)
+    #PortFenster =Label(root,text = PortEntry.get(), fg="black", bg="white")
+    #PortFenster.place(y=30, x=200, width= 100, height= 30)
         
     SOCFenster =Label(root,text = SOCEntry.get(), fg="black", bg="white")
     SOCFenster.place(y=80, x=200, width= 100, height= 30)
@@ -91,24 +98,31 @@ def print_entry():
     ConnectionFenster =Label(root,text = "connected", fg="black", bg="white")
     ConnectionFenster.place(y=200, x=90, width= 120, height= 30)
             
+    global counter
+    counter=counter+1
+    if(counter>300):
+        counter=0
 
-    server.data_bank.set_input_registers(7, [SOCEntry.get()])
-    server.data_bank.set_input_registers(8, [SOHEntry.get()])
-    server.data_bank.set_input_registers(9, [EntladeEntry.get()])
-    server.data_bank.set_input_registers(10, [LadeEntry.get()])
+
+    server.data_bank.set_input_registers(168, [SOCEntry.get()])
+    server.data_bank.set_input_registers(169, [SOHEntry.get()])
+    server.data_bank.set_input_registers(171, [EntladeEntry.get()])
+    server.data_bank.set_input_registers(172, [LadeEntry.get()])
+    server.data_bank.set_input_registers(173, [counter])
     time.sleep(1)
+    
                
     #except:
         #server.stop()
         #print("Server offline")
         
 
-    print("Server is run:" + str(server.is_run)) 
-    
-    print("Value after writing:" + str(server.data_bank.get_input_registers(9)))
+    print("Server is run:" + str(server.is_run))  
+    print("Value after writing:" + str(server.data_bank.get_input_registers(168)))
                   
-   
+    root.after(30000, print_entry) 
  
+    
 #Button to entry print_entry()
 #------------------------------------------------------------------------------
 Button2= tk.Button(text="Write Data to Server",command= print_entry)
@@ -119,5 +133,5 @@ Button2.place(y=230, x=90,width= 120,height= 30)
 QuitButton = tk.Button(text="Close Programm", command=root.destroy)    
 QuitButton.place(y=260, x=90,width= 120,height= 30)
 
-        
+root.after(30000, print_entry)        
 root.mainloop()
